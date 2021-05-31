c16c7ee5869d0158cf38bd1c819a868e7f293182
Victor Hüni <victor.hueni@protonmail.com>
2021-05-31 21:53:54 +0200

    setting up dark mode toggle button + js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('testing')
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+
+//Switch dynamically
+function switchTheme(event){
+    if(event.target.checked){
+        document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }
+}
+
+
+// Event Listener
+toggleSwitch.addEventListener('change', switchTheme);
