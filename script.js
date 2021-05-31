0835b49d27bed4d393579a8e48a3b8ac4a40079b
Victor Hüni <victor.hueni@protonmail.com>
2021-05-31 22:49:06 +0200

    adding localStorage features to store the user preference

--- script.js
+++ script.js
@@ -15,33 +15,45 @@ function toggleMode(mode){
     if(mode == 'dark'){
         navBackgroundColor = 'rgb(0 0 0 / 50%)';
         textBoxBackgroundColor = 'rgb(255 255 255 / 50%)';
-        toggleIcon.children[1].classList.replace('fa-moon','fa-light');
-   } else {
+        toggleIcon.children[1].classList.replace('fa-sun','fa-moon');
+    } else {
         navBackgroundColor = 'rgb(255 255 255 / 50%)';
         textBoxBackgroundColor = 'rgb(0 0 0 / 50%)';
-        toggleIcon.children[1].classList.replace('fa-sun','fa-moon');
-   }
+        toggleIcon.children[1].classList.replace('fa-moon','fa-sun');
+    }
 
-   nav.style.backgroundColor = navBackgroundColor;
-   textBox.style.backgroundColor = textBoxBackgroundColor;
-   toggleIcon.children[0].textContent = `${mode.charAt(0).toUpperCase() + mode.slice(1)} Mode`;
-   image1.src = `img/undraw_proud_coder_${mode}.svg`;
-   image2.src = `img/undraw_feeling_proud_${mode}.svg`;
-   image3.src = `img/undraw_conceptual_idea_${mode}.svg`;
-   
+    nav.style.backgroundColor = navBackgroundColor;
+    textBox.style.backgroundColor = textBoxBackgroundColor;
+    toggleIcon.children[0].textContent = `${mode.charAt(0).toUpperCase() + mode.slice(1)} Mode`;
+    image1.src = `img/undraw_proud_coder_${mode}.svg`;
+    image2.src = `img/undraw_feeling_proud_${mode}.svg`;
+    image3.src = `img/undraw_conceptual_idea_${mode}.svg`;
+
 }
 
 //Switch dynamically
 function switchTheme(event){
+    let mode = '';
     if(event.target.checked){
-        document.documentElement.setAttribute('data-theme', 'dark');
-        toggleMode('dark');
+        mode = 'dark';
     } else {
-        document.documentElement.setAttribute('data-theme', 'light');
-        toggleMode('light');
+        mode = 'light';
     }
+    document.documentElement.setAttribute('data-theme', mode);
+    localStorage.setItem('theme', mode);
+    toggleMode(mode);
 }
 
 
 // Event Listener
 toggleSwitch.addEventListener('change', switchTheme);
+
+// Check local storage for theme
+const currentTheme = localStorage.getItem('theme');
+if(currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if(currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        toggleMode(currentTheme);
+    }
+}
