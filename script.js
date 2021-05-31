d41dde8aadee4938da8265c52eb6ced6675d659c
Victor Hüni <victor.hueni@protonmail.com>
2021-05-31 23:13:06 +0200

    basic code review

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const DARK_THEME = 'dark'
+const LIGHT_THEME = 'light'
+const USER_THEME = 'user_theme'
+
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
 const nav = document.getElementById('nav');
 const toggleIcon = document.getElementById('toggle-icon');
@@ -7,41 +11,38 @@ const image3 = document.getElementById('image3');
 const textBox= document.getElementById('text-box');
 
 
-function toggleMode(mode){
-    
-    let navBackgroundColor = '';
-    let textBoxBackgroundColor = '';
+function toggleMode(themeToApply){
 
-    if(mode == 'dark'){
-        navBackgroundColor = 'rgb(0 0 0 / 50%)';
-        textBoxBackgroundColor = 'rgb(255 255 255 / 50%)';
-        toggleIcon.children[1].classList.replace('fa-sun','fa-moon');
-    } else {
-        navBackgroundColor = 'rgb(255 255 255 / 50%)';
-        textBoxBackgroundColor = 'rgb(0 0 0 / 50%)';
-        toggleIcon.children[1].classList.replace('fa-moon','fa-sun');
-    }
+    let isDark = themeToApply == DARK_THEME;
 
+    let navBackgroundColor =  isDark ? 'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 / 50%)';
+    let textBoxBackgroundColor =  isDark ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)';
+    isDark ? toggleIcon.children[1].classList.replace('fa-sun','fa-moon') : toggleIcon.children[1].classList.replace('fa-moon','fa-sun');
     nav.style.backgroundColor = navBackgroundColor;
     textBox.style.backgroundColor = textBoxBackgroundColor;
-    toggleIcon.children[0].textContent = `${mode.charAt(0).toUpperCase() + mode.slice(1)} Mode`;
-    image1.src = `img/undraw_proud_coder_${mode}.svg`;
-    image2.src = `img/undraw_feeling_proud_${mode}.svg`;
-    image3.src = `img/undraw_conceptual_idea_${mode}.svg`;
+    toggleIcon.children[0].textContent = `${themeToApply.charAt(0).toUpperCase() + themeToApply.slice(1)} Mode`;
+    image1.src = `img/undraw_proud_coder_${themeToApply}.svg`;
+    image2.src = `img/undraw_feeling_proud_${themeToApply}.svg`;
+    image3.src = `img/undraw_conceptual_idea_${themeToApply}.svg`;
 
 }
 
 //Switch dynamically
 function switchTheme(event){
-    let mode = '';
+
+    //Default Mode is Light
+    let themeToApply = LIGHT_THEME;
+
     if(event.target.checked){
-        mode = 'dark';
-    } else {
-        mode = 'light';
+        themeToApply = DARK_THEME;
     }
-    document.documentElement.setAttribute('data-theme', mode);
-    localStorage.setItem('theme', mode);
-    toggleMode(mode);
+
+    //Apply theme
+    document.documentElement.setAttribute('data-theme', themeToApply);
+    toggleMode(themeToApply);
+
+    //Save user preference
+    localStorage.setItem(USER_THEME, themeToApply);   
 }
 
 
@@ -49,10 +50,12 @@ function switchTheme(event){
 toggleSwitch.addEventListener('change', switchTheme);
 
 // Check local storage for theme
-const currentTheme = localStorage.getItem('theme');
+const currentTheme = localStorage.getItem(USER_THEME);
 if(currentTheme) {
+
     document.documentElement.setAttribute('data-theme', currentTheme);
-    if(currentTheme === 'dark') {
+
+    if(currentTheme == DARK_THEME) {
         toggleSwitch.checked = true;
         toggleMode(currentTheme);
     }
